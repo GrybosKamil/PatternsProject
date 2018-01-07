4599dd16d22fccbceed78f054206be48a1a35395
Kamil Grybos <grybos.kamil@gmail.com>
2018-01-07 10:40:33 -0800

    Add h2 database with example customer

--- backend/src/main/java/com/grybos/kamil/patternsproject/Application.java
+++ backend/src/main/java/com/grybos/kamil/patternsproject/Application.java
@@ -4,6 +4,10 @@ import java.lang.reflect.Method;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -18,10 +22,15 @@ import org.springframework.web.servlet.mvc.condition.PatternsRequestCondition;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
-@SpringBootApplication
-public class Application {
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import com.grybos.kamil.patternsproject.resources.Customer;
 
-    private static Logger logger = LoggerFactory.getLogger(Application.class);
+@SpringBootApplication
+public class Application implements CommandLineRunner{
+
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
     @Value("${rest.api.base.path}")
     private String restApiBasePath;
@@ -70,5 +79,35 @@ public class Application {
         };
     }
 
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void run(String... strings) throws Exception {
+
+        logger.info("Creating tables");
+
+        jdbcTemplate.execute("DROP TABLE customers IF EXISTS");
+        jdbcTemplate.execute("CREATE TABLE customers(" +
+                "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
+
+        // Split up the array of whole names into an array of first/last names
+        List<Object[]> splitUpNames = Arrays.asList("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long").stream()
+                .map(name -> name.split(" "))
+                .collect(Collectors.toList());
+
+        // Use a Java 8 stream to print out each tuple of the list
+        splitUpNames.forEach(name -> logger.info(String.format("Inserting customer record for %s %s", name[0], name[1])));
+
+        // Uses JdbcTemplate's batchUpdate operation to bulk load data
+        jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
+
+        logger.info("Querying for customer records where first_name = 'Josh':");
+        jdbcTemplate.query(
+                "SELECT id, first_name, last_name FROM customers WHERE first_name = ?", new Object[] { "Josh" },
+                (rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name"))
+        ).forEach(customer -> logger.info(customer.toString()));
+    }
 }
 
